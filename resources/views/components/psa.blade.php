33a41c1d41611bb15c6243fb6365c58c06528273
Tramaine Jackson <jackson.tramaine3@gmail.com>
2025-05-04 20:36:02 -0400

    Edited announcement

--- resources/views/components/psa.blade.php
+++ resources/views/components/psa.blade.php
@@ -1,6 +1,6 @@
 <div class="container rounded rounded-5 py-3 mb-5" id="" style="background-color: #cfb8ff;">
     <div class="row align-items-center" id="">
-        <div class="col" id="">
+        <div class="col-12 col-md-6 mb-3 mb-md-0" id="">
             <div class="container" id="">
                 <div class="row" id="">
                     <div class="col-12" id="">
@@ -8,18 +8,18 @@
                     </div>
 
                     <div class="col-12" id="">
-                        <p class="">We’re excited to start planning the 2026 Jackson-Green Family Reunion happening July 30 – August 2 in Norfolk, VA! 🎉
+                        <p class="">We’re excited to start planning the 2026 Jackson-Green Family Reunion happening <b>July 30 – August 2 in Norfolk, VA!</b> 🎉
                             Please take a moment to complete the short survey linked below. It helps us gather updated contact information and get a better idea of the types of activities you’d enjoy while we're all together.</p>
                     </div>
 
-                    <div class="col-12" id="">
+                    <div class="col-12 text-center text-md-start" id="">
                         <a href="https://docs.google.com/forms/d/e/1FAIpQLScxb5YM4VmZo_cH1Nj7YemAmQlEh1nnzW82vDJa9grLxM2How/viewform" class="btn btn-lg btn-secondary">Jackson Green Reunion Survey</a>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="col text-end my-auto" id="">
+        <div class="col-12 col-md-6 text-center text-md-end my-auto" id="">
             <img src="{{ asset('images/announcement_character_1.png') }}" class="img-fluid" alt="Announcement Character" style="max-height: 400px;">
         </div>
     </div>
