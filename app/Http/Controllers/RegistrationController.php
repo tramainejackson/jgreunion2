7c439630bf03e404450deb78454a56b28e9247b1
Tramaine Jackson <jackson.tramaine3@gmail.com>
2025-06-19 15:40:45 -0400

    Removed registration form because of spam

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -27,7 +27,7 @@ class RegistrationController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except(['store', 'guest_registration']);
+        $this->middleware('auth')->except(['store']);
     }
 
     /**
