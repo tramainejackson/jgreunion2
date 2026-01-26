108ec490bc37bbec32d953e0a0a7f6278307872e
Tramaine Jackson <jackson.tramaine3@gmail.com>
2026-01-26 12:59:06 -0500

    Updated reset password reset form

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,6 +48,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'username' => $request->username,
+            'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
